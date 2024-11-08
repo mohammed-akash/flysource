23b3dfdf812c579458ae98cac24bd0d706c714a8
mohammed-akash <mdakash2003evergreen@gmail.com>
2024-11-09 02:10:25 +0530

    updated

--- src/components/Common/ServiceHighLight.jsx
+++ src/components/Common/ServiceHighLight.jsx
@@ -6,8 +6,8 @@ const ServiceHighLight = ({ Icon, title, desc }) => {
             <div className="h-12 w-12 flex items-center justify-center border rounded-full mb-3 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#3f3fc2b4] via-[#3f3fc2dc] to-[#3f3fc232]">
                 <Icon className='text-2xl text-white' />
             </div>
-            <h3 className="text-2xl text-[#1d1c1c] text-start font-[500] mb-2">{title}</h3>
-            <p className="text-[#3c4b62] text-start">{desc}</p>
+            <h3 className="text-2xl text-[#1d1c1c] md:text-start text-center font-[500] mb-2">{title}</h3>
+            <p className="text-[#3c4b62] md:text-start text-center">{desc}</p>
         </div>
     )
 }
